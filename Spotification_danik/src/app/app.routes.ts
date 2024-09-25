467329f74a4b90bf61f7aa8b8b027341401f82ec
Danik <2290726@cegepmontpetit.ca>
2024-09-25 15:11:08 -0400

    FCT et BUGFIX : Nouveau projet angular et Ajout HTML/CSS
    Mon projet angular de départ ne se lancait plus, la solution a été de recommencer un nouveau projet et de copier coller les fichiers restants.
    J'ai également terminé de copier coller les fichiers html et css restants

---
+++ Spotification_danik/src/app/app.routes.ts
@@ -0,0 +1,13 @@
+import { Routes } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
+import { AlbumsComponent } from './albums/albums.component';
+import { ChansonsComponent } from './chansons/chansons.component';
+import { ConcertsComponent } from './concerts/concerts.component';
+
+export const routes: Routes = [
+    {path:"", redirectTo:"/accueil", pathMatch:"full"},
+    {path:"accueil", component: AccueilComponent},
+    {path:"albums", component: AlbumsComponent},
+    {path:"chansons", component: ChansonsComponent},
+    {path:"concerts", component: ConcertsComponent}
+];
---
+++ Spotification_danik/src/app/chansons/chansons.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chansons',
+  standalone: true,
+  imports: [],
+  templateUrl: './chansons.component.html',
+  styleUrl: './chansons.component.css'
+})
+export class ChansonsComponent {
+
+}
