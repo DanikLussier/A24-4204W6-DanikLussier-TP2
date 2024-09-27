72fd9a95ed36bb222dcf9406ddf6b26f7f87d644
Danik <2290726@cegepmontpetit.ca>
2024-09-27 12:59:38 -0400

    PROGRES : Fonctionnement accueil
    Affichage des artistes complétée.
    Recherche des artistes fonctionnelle.
    Sauvegarde dans le stockage local fonctionnelle
    Ajout d'un bouton qui permet de vider la liste des artistes
    Manque Seulement les boutons qui redirigent vers les concerts et albums

--- Spotification_danik/src/app/accueil/accueil.component.ts
+++ Spotification_danik/src/app/accueil/accueil.component.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { SpotifyService } from '../services/spotify.service';
 import { Artist } from '../models/artist';
-import { CommonModule } from '@angular/common';
+import { CommonModule, JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-accueil',
@@ -15,26 +15,38 @@ import { CommonModule } from '@angular/common';
 export class AccueilComponent {
 
   artistName = ""
-  public artists : Array<Artist>
+  artists = new Array<Artist>
 
-  constructor(public spotify : SpotifyService) {
-    //plus tard on va l'initialiser avec le localstorage
-    this.artists = new Array<Artist>
-
-
-  }
+  constructor(public spotify : SpotifyService) {}
 
   ngOnInit() {
     this.spotify.connect()
-    console.log(this.artists)
+    this.artists = this.GetArtists()
   }
 
-  AddArtist() {
-    this.spotify.searchArtist(this.artistName).then((a) => {
+  async AddArtist() {
+    await this.spotify.searchArtist(this.artistName).then((a) => {
       this.artists.push(a)
     }).catch((error) => {
       console.log("No artist found")
     })
-    console.log(this.artists)
+    this.SaveArtist()
+  }
+
+  SaveArtist() {
+    localStorage.setItem("artists", JSON.stringify(this.artists))
+  }
+
+  GetArtists(): Array<Artist> {
+    var jsonArtists = localStorage.getItem("artists") 
+    if (jsonArtists != null) {
+      return JSON.parse(jsonArtists)
+    }
+    return new Array<Artist>
+  }
+
+  ClearArtists() {
+    this.artists = new Array<Artist>
+    this.SaveArtist()
   }
 }
