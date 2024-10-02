4af4b10a586d550ce8c2cb5c1a2cb9501f9dd7d1
Danik <2290726@cegepmontpetit.ca>
2024-10-02 17:49:55 -0400

    PROGRES : Fonctionnement des concerts
    Ajout des concerts dans la liste complété
    Ajout des méthodes du service de BandsInTown.
    Configuration de la Google Map et affichage de celle-ci. Ajout de deux variables dans Concert pour la lat. et la lng.
    Manque seulement de placer les points sur la map et de faire l'internationalisation

--- Spotification_danik/src/app/concerts/concerts.component.ts
+++ Spotification_danik/src/app/concerts/concerts.component.ts
@@ -4,6 +4,7 @@ import { SpotifyService } from '../services/spotify.service';
 import { GoogleMapsModule } from '@angular/google-maps';
 import { CommonModule } from '@angular/common';
 import { BandsInTownService } from '../services/bands-in-town.service';
+import { Concert } from '../models/concert';
 
 @Component({
   selector: 'app-concerts',
@@ -16,12 +17,9 @@ export class ConcertsComponent {
 
   center : google.maps.LatLngLiteral = {lat: 42, lng:-4}
   zoom : number = 5
-  markerPositions : google.maps.LatLngLiteral[] = [
-    {lat: 42, lng: -4},
-    {lat: 40, lng: -0},
-    {lat: 48, lng: -8}
-  ]
+  markerPositions : google.maps.LatLngLiteral[] = []
 
+  concerts : Concert[] = new Array<Concert>
   artistName : string | null = ""
 
   constructor(public spotify : SpotifyService, public bandsInTown : BandsInTownService, public route : ActivatedRoute) {}
@@ -32,7 +30,21 @@ export class ConcertsComponent {
   }
 
   async GetEvents() {
-    if (this.artistName != null)
-      await this.bandsInTown.GetEvents(this.artistName)
+    if (this.artistName != null) {
+      await this.bandsInTown.GetEvents(this.artistName).then(async (concerts) => {
+        this.concerts = concerts
+        await this.InitPoints()
+      }).catch((err) => {
+        console.log("Error while searching for shows")
+      })
+    }
+  }
+
+  async InitPoints() {
+    for (let i = 0; i < this.concerts.length; i++)
+    {
+      this.markerPositions.push({lat: this.concerts[i].lat, lng: this.concerts[i].lng})
+      console.log(this.concerts[i].lat, this.concerts[i].lng)
+    }
   }
 }
--- Spotification_danik/src/app/services/bands-in-town.service.ts
+++ Spotification_danik/src/app/services/bands-in-town.service.ts
@@ -2,6 +2,8 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Concert } from '../models/concert';
+import { GoogleMapsModule } from '@angular/google-maps';
+import { Chanson } from '../models/chansons';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +12,7 @@ export class BandsInTownService {
 
   constructor(public http : HttpClient) { }
 
-  async GetEvents(artistName : string) {
+  async GetEvents(artistName : string): Promise<Concert[]> {
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json'
@@ -20,17 +22,20 @@ export class BandsInTownService {
     try {
       let x = await lastValueFrom(this.http.get<any>("https://rest.bandsintown.com/artists/" + artistName + "/events?app_id=2b32475766802ac01eefda45e9e42ea0%20&date=all" + httpOptions))
       console.log(x)
+
+      let concerts = new Array<Concert>
+      for (let i = 0; i < x.length; i++)
+      {
+        concerts.push(new Concert(x[i].datetime, x[i].venue.country, x[i].venue.city, x[i].venue.latitude, x[i].venue.longitude))
+      }
+
+      return concerts
     }
     catch (err) {
       console.log("Erreur en cherchant les concerts")
       console.log(err)
     }
 
-    // for (let i = 0; i < x.items.length; i++)
-    // {
-
-    // }
-
-    // return 
+    return new Array<Concert>
   }
 }
